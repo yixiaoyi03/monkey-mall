37bb856066c655919732885fa7affee41da3fca8
shuyi <yixiaoyi03@gmail.com>
2024-07-28 23:21:40 +0800

    工程初步搭建

---
+++ monkey-common/common-file/src/main/java/com/lingyi/common/file/vo/FileInfo.java
@@ -0,0 +1,12 @@
+package com.lingyi.common.file.vo;
+
+import lombok.Data;
+
+@Data
+public class FileInfo {
+
+    private String name;
+
+    private String url;
+
+}
---
+++ monkey-common/common-web/src/main/java/com/lingyi/common/web/exception/BizException.java
@@ -0,0 +1,35 @@
+package com.lingyi.common.web.exception;
+
+import com.lingyi.common.result.IResultCode;
+import lombok.Getter;
+
+/**
+ * 自定义业务异常
+ *
+ * @author haoxr
+ * @date 2022/7/31
+ */
+@Getter
+public class BizException extends RuntimeException {
+
+    public IResultCode resultCode;
+
+    public BizException(IResultCode errorCode) {
+        super(errorCode.getMsg());
+        this.resultCode = errorCode;
+    }
+
+    public BizException(String message){
+        super(message);
+    }
+
+    public BizException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public BizException(Throwable cause){
+        super(cause);
+    }
+
+
+}
